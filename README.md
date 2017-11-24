5c539bb7d8ca4fc888f91708f970e0972138a1dc
mitchell broome <mbroome@unknown00155D014000.attlocal.net>
2017-11-23 21:15:55 -0500

    Start scraping aquabid to convert it's data into a usable format

---
+++ README.md
@@ -0,0 +1,7 @@
+ Need phantomjs to scrape aquabid.
+
+```shell
+phantomjs  lib/pollers/active-auctions.js >active.html
+bin/active.py > report.json
+```
+
---
+++ bin/active.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+import json
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+titlePattern = re.compile('<tr .*<b>(.*)<\/b.*tr>')
+itemPattern = re.compile('<tr .*?><td .*?>.*<a href=\"(http:\/\/.*?)\">(.*?)<\/a>.*?(<img .*>)<\/font></td><td .*?>.*?>(.*?)<\/font.*?</td><td .*?><font.*?>(.*?)<\/font.*></td><td .*?><font .*?>(.*?)<\/.*</td><td .*?><font.*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td><\/tr>')
+itemNoImagePattern = re.compile('<tr .*?><td .*?>.*<a href=\"(http:\/\/.*?)\">(.*?)<\/a>.*?<\/font></td><td .*?>.*?>(.*?)<\/font.*?</td><td .*?><font.*?>(.*?)<\/font.*></td><td .*?><font .*?>(.*?)<\/.*</td><td .*?><font.*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td><\/tr>')
+
+content = open('active.html', 'r').read()
+section = ''
+found = False
+
+output = []
+for line in content.split('\n'):
+   if '<tbody>' in line and 'Item' in line:
+      found = True
+   if found:
+      record = {
+                'id': '',
+                'section': section,
+                'link': '',
+                'item': '',
+                'pic': '',
+                'seller': '',
+                'closes': '',
+                'bid': '',
+                'price': '',
+                'reserve': '',
+                'shipping': '',
+               }
+
+      if '</tbody></table>' in line:
+         found = False
+      else:
+         line = line.replace('&amp;', '&')
+         line = line.replace('&nbsp;', ' ')
+         line = line.replace('nbsp', ' ')
+         m = itemPattern.match(line)
+         if m:
+            record['link'] = m.group(1)
+            record['item'] = m.group(2)
+            record['pic'] = m.group(3)
+            record['seller'] = m.group(4)
+            record['closes'] = m.group(5)
+            record['bid'] = m.group(6)
+            record['price'] = m.group(7)
+            record['reserve'] = m.group(8)
+         else:
+            t = titlePattern.match(line)
+            if t:
+               section = t.group(1)
+            else:
+               i = itemNoImagePattern.match(line)
+               if i:
+                  record['link'] = i.group(1)
+                  record['item'] = i.group(2)
+                  record['seller'] = i.group(3)
+                  record['closes'] = i.group(4)
+                  record['bid'] = i.group(5)
+                  record['price'] = i.group(6)
+                  record['reserve'] = i.group(7)
+
+               else:
+                  if '<tbody>' not in line:
+                     print 'XXXX: %s' % line
+
+         if record['item']:
+            # sanatize the data since it's all over the place
+            id = record['link'][record['link'].find('?') + 1:]
+            record['id'] = id
+
+            if 'buyitnow' in record['bid']:
+               record['bid'] = 'buyitnow'
+            elif 'No Bids' in record['bid']:
+               record['bid'] = '0'
+
+            picData = record['pic']
+            if '/us.gif' in picData:
+               record['shipping'] = 'us'
+            elif '/intlship.gif' in picData:
+               record['shipping'] = 'intl'
+            elif '/pu.gif' in picData:
+               record['shipping'] = 'pickup'
+
+            if 'PIC' in record['pic']:
+               record['pic'] = True
+            else:
+               record['pic'] = False
+
+            record['price'] = record['price'].rstrip()
+            record['price'] = record['price'].replace('$', '')
+            try:
+               # why can there sometimes be no price...
+               f = float(record['price'])
+               record['price'] = f
+            except Exception, e:
+               pass
+
+            #pp.pprint(record)
+            output.append(record)
+
+print json.dumps(output)
+
