d7df81206b4ea4ee3ac3a8b56eba2dae48faaf3d
Mitchell Broome <mbroome@sc3271.lan>
2020-06-21 11:02:21 -0400

    updated the active poller

--- bin/active.py
+++ bin/active.py
@@ -4,6 +4,7 @@ import sys
 import re
 import json
 import time
+import requests
 
 scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
 sys.path.append(scriptPath + '/../lib')
@@ -13,18 +14,52 @@ import timetools
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-titlePattern = re.compile('<tr .*<b>(.*)<\/b.*tr>')
-descriptionPattern = re.compile('<tr .*?><td .*?>.*<a href=\"(http:\/\/.*?)\">(.*?)<\/a>.*?(<img .*>)<\/font></td><td .*?>.*?>(.*?)<\/font.*?</td><td .*?><font.*?>(.*?)<\/font.*></td><td .*?><font .*?>(.*?)<\/.*</td><td .*?><font.*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td><\/tr>')
-descriptionNoImagePattern = re.compile('<tr .*?><td .*?>.*<a href=\"(http:\/\/.*?)\">(.*?)<\/a>.*?<\/font></td><td .*?>.*?>(.*?)<\/font.*?</td><td .*?><font.*?>(.*?)<\/font.*></td><td .*?><font .*?>(.*?)<\/.*</td><td .*?><font.*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td><\/tr>')
+# <TR bgcolor=#C0C0C0><TD colspan=6><font size=2 face=Arial>&nbsp;&nbsp;<B>Air Pumps</B></TD></TR>
+titlePattern = re.compile('<tr .*<b>(.*)<\/b.*tr>', re.IGNORECASE)
 
-content = open('active.html', 'r').read()
+# <TR BGCOLOR=#FFFFFF onMouseOver="if (!window.__cfRLUnblockHandlers) return false; this.style.backgroundColor='#BACCDE'; this.style.cursor='hand';" onMouseOut="if (!window.__cfRLUnblockHandlers) return false; this.style.backgroundColor=&quot;#FFFFFF&quot;" data-cf-modified-a45195ffae2abc17658d0216-=""><TD valign=top><font face=arial SIZE=2><A HREF="https://www.aquabid.com/cgi-bin/auction/auction.cgi?airpumps&1592785802" target=_new><img src="https://www.aquabid.com/images/newwindow.gif" border=0 alt="Open In New Window"></a>&nbsp;<A HREF="https://www.aquabid.com/cgi-bin/auction/auction.cgi?airpumps&1592785802">12 AIR STONES-TOP QUALITY-BARGAIN PRICE!! </A> <IMG align="top" border="0" SRC="https://www.aquabid.com/images/pic.gif" alt="PIC "> <IMG align="top" border="0" SRC="https://www.aquabid.com/images/us.gif" alt="Will Ship to United States Only"></TD><TD ALIGN=CENTER WIDTH=12%><font face=arial SIZE=2>Buyguy52</TD><TD ALIGN=RIGHT><font face=arial SIZE=2>6/21</TD><TD ALIGN=RIGHT><font face=arial SIZE=2><IMG SRC="https://www.aquabid.com/images/buyitnow.gif" alt="Buy it now!"></TD><TD ALIGN=RIGHT><font face=arial SIZE=2>$4.00 <IMG align="top" SRC="https://www.aquabid.com/images/icons/reserve_none.gif" border="0 alt=" No Reserve "></TD></TR>
+
+# <TR BGCOLOR=#FFFFFF onMouseOver="if (!window.__cfRLUnblockHandlers) return false; this.style.backgroundColor='#BACCDE'; this.style.cursor='hand';" onMouseOut="if (!window.__cfRLUnblockHandlers) return false; this.style.backgroundColor=&quot;#FFFFFF&quot;" data-cf-modified-a45195ffae2abc17658d0216-=""><TD valign=top><font face=arial SIZE=2><A HREF="https://www.aquabid.com/cgi-bin/auction/auction.cgi?airpumps&1592785802" target=_new><img src="https://www.aquabid.com/images/newwindow.gif" border=0 alt="Open In New Window"></a>&nbsp;
+# <A HREF="https://www.aquabid.com/cgi-bin/auction/auction.cgi?airpumps&1592785802">
+# 12 AIR STONES-TOP QUALITY-BARGAIN PRICE!! 
+# </A> <IMG align="top" border="0" SRC="https://www.aquabid.com/images/pic.gif" alt="PIC "> 
+# <IMG align="top" border="0" SRC="https://www.aquabid.com/images/us.gif" alt="Will Ship to United States Only"></TD><TD ALIGN=CENTER WIDTH=12%>
+# <font face=arial SIZE=2>Buyguy52</TD><TD ALIGN=RIGHT>
+# <font face=arial SIZE=2>6/21</TD><TD ALIGN=RIGHT>
+# <font face=arial SIZE=2><IMG SRC="https://www.aquabid.com/images/buyitnow.gif" alt="Buy it now!"></TD><TD ALIGN=RIGHT>
+# <font face=arial SIZE=2>$4.00 <IMG align="top" SRC="https://www.aquabid.com/images/icons/reserve_none.gif" border="0 
+# alt=" No Reserve "></TD></TR>
+#descriptionPattern = re.compile('<tr .*?><td .*?>.*<a href=\"(https:\/\/.*?)\">(.*?)<\/a>.*?(<img .*>)<img .*SRC="(.*)" alt.*<td .*?><font.*?>(.*?)<\/td><td .*?><font .*?>(.*?)<\/.*</td><td .*?><font.*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td><\/tr>')
+
+#                                                             link             desc             img                   seller               close                    bids                  price                reserve
+descriptionPattern = re.compile('<tr .*?><td .*?>.*<a href=\"(https:\/\/.*?)\">(.*?)<\/a> +<img (.*)><\/td.*?<font .*?>(.*?)<\/td.*?<font .*?>(.*?)</td.*?<font .*?>(.*?)</td.*?<font .*?>(.*?)<img.*\/reserve_(.*)\.gif.*?</td', re.IGNORECASE)
+
+
+url = 'https://www.aquabid.com/cgi-bin/auction/search.cgi'
+
+postData = {
+   'searchstring': '.*',
+   'searchtype': 'keyword',
+   'category': 'all',
+   'searchloc': 'All'
+}
+
+r = requests.post(url, postData)
+
+#print r.text
+
+#sys.exit(0)
+
+#content = open('active.html', 'r').read()
+content = r.text
 section = ''
 found = False
 
 activeList = []
 bidList = []
 for line in content.split('\n'):
-   if '<tbody>' in line and 'Item' in line:
+   # find the start of the content in the body
+   if '<B>Item</B>' in line:
       found = True
    if found:
       record = {
@@ -41,7 +76,8 @@ for line in content.split('\n'):
                 'shipping': '',
                }
 
-      if '</tbody></table>' in line:
+      
+      if '>Policies</a>' in line:
          found = False
       else:
          line = line.replace('&amp;', '&')
@@ -61,23 +97,13 @@ for line in content.split('\n'):
             t = titlePattern.match(line)
             if t:
                section = t.group(1)
-            else:
-               i = descriptionNoImagePattern.match(line)
-               if i:
-                  record['link'] = i.group(1)
-                  record['description'] = i.group(2)
-                  record['seller'] = i.group(3)
-                  record['closes'] = i.group(4)
-                  record['bids'] = i.group(5)
-                  record['price'] = i.group(6)
-                  record['reserve'] = i.group(7)
 
-               else:
-                  if '<tbody>' not in line:
-                     print 'XXXX: %s' % line
-
+         #pp.pprint(record)
+         
          if record['description']:
             # sanatize the data since it's all over the place
+            record['description'] = record['description'].rstrip().lstrip()
+
             id = record['link'][record['link'].find('?') + 1:]
             id = id[id.find('&') + 1:]
             category = record['link'][record['link'].find('?') + 1:]
@@ -103,6 +129,8 @@ for line in content.split('\n'):
                record['shipping'] = 'intl'
             elif '/pu.gif' in picData:
                record['shipping'] = 'pickup'
+            if '/contus.gif' in picData:
+               record['shipping'] = 'contus'
 
             if 'PIC' in record['pic']:
                record['pic'] = True
@@ -118,6 +146,10 @@ for line in content.split('\n'):
             except Exception, e:
                pass
 
+         #pp.pprint(record)
+         #print '####'
+
+         
             #pp.pprint(record)
             activeList.append(record)
             try:
@@ -127,6 +159,7 @@ for line in content.split('\n'):
                   bidList.append(record)
             except:
                pass
+         
 
 print json.dumps(activeList)
 #print json.dumps(bidList)
